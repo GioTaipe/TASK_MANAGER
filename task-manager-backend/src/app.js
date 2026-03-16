84e8b18ac8b39ffcf8a7f8f1e97ae9a71a980316
GioTaipe <120596675+GioTaip@users.noreply.github.com>
2026-03-16 16:43:10 +0100

    cors

--- task-manager-backend/src/app.js
+++ task-manager-backend/src/app.js
@@ -4,17 +4,17 @@ const app = express();
 const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes'); 
 
-
-app.use(express.json());
 app.use(cors({
   origin: [
     'https://task-manager.up.railway.app',
     'https://task-manager.up.railway.app/api',
     'https://task-manager-2-wvfr.onrender.com'
   ],
+  credentials: true,
   allowedHeaders: ['Content-Type', 'x-device-id'],
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
 }));
+app.use(express.json());
 app.use('/api', taskRoutes);
 
 module.exports = app;
