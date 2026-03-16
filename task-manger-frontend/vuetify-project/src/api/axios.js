9d6469e56c5ddf4e358601c8c03659bd4ee59042
GioTaipe <120596675+GioTaip@users.noreply.github.com>
2026-03-16 17:07:23 +0100

    trigger redeploy

--- task-manger-frontend/vuetify-project/src/api/axios.js
+++ task-manger-frontend/vuetify-project/src/api/axios.js
@@ -12,7 +12,7 @@ const getDeviceId = () => {
 };
 
 const api = axios.create({
-  baseURL: "taskmanager-production-2e20.up.railway.app/api",
+  baseURL: "https://taskmanager-production-2e20.up.railway.app/api",
 });
 
 // 2. Interceptor de Petición
